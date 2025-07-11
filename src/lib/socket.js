f12aa19ea496f84b74ae1b7af97132e836b66538
saipavanvangapalli <saipavanvangapalli@gmail.com>
2025-07-11 18:46:08 +0530

    addded fromotend

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,16 +7,20 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://chat-frontend-eight-alpha.vercel.app"
+    ],
+    credentials: true,
   },
 });
+
+// Store online users
+const userSocketMap = {}; // { userId: socketId }
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
-
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
@@ -24,7 +28,6 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
